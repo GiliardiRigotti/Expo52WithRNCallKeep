13b4498ee65da7eef8957aa16ae7646d06c78b2f
Giliardi Rigotti <giliardi.oliveira@gmail.com>
2024-12-11 13:53:54 -0300

    Configured RNCallKeep

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import {
+	PermissionsAndroid,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import RNCallKeep from "react-native-callkeep";
+import * as Crypto from "expo-crypto";
+
+const options = {
+	ios: {
+		appName: "My app name",
+	},
+	android: {
+		alertTitle: "Permissions required",
+		alertDescription: "This application needs to access your phone accounts",
+		cancelButton: "Cancel",
+		okButton: "ok",
+		imageName: "phone_account_icon",
+		additionalPermissions: [PermissionsAndroid.PERMISSIONS.example],
+		// Required to get audio in background when using Android 11
+		foregroundService: {
+			channelId: "com.giliardi.expo52",
+			channelName: "Foreground service for my app",
+			notificationTitle: "My app is running on background",
+			notificationIcon: "Path to the resource icon of the notification",
+		},
+	},
+};
+
+RNCallKeep.setup(options).then((accepted) => {});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const startCall = () => {
+		const uuid = Crypto.randomUUID();
+		try {
+			RNCallKeep.displayIncomingCall(uuid, "00000", "000000", "number", false);
+			console.log(uuid);
+		} catch (error) {
+			console.log(error);
+		}
+
+		/* RNCallKeep.startCall('99800870-a236-40d8-a550-294179ff6959', '0000000000', 'Jhon', 'number', false);
+    RNCallKeep.updateDisplay('99800870-a236-40d8-a550-294179ff6959', 'Jhon', '0000000000') */
+	};
+
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity onPress={startCall} style={styles.button}>
+				<Text style={styles.title}>Start Call</Text>
+			</TouchableOpacity>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "white",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	button: {
+		width: "80%",
+		height: 50,
+		borderRadius: 10,
+		backgroundColor: "#d2d2d2",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 25,
+		fontWeight: "700",
+		color: "black",
+	},
 });
---
+++ ios/expo52/AppDelegate.mm
@@ -0,0 +1,75 @@
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTLinkingManager.h>
+#import "RNCallKeep.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"main";
+
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
+  [RNCallKeep setup:@{
+    @"appName": @"Awesome App",
+    @"maximumCallGroups": @3,
+    @"maximumCallsPerCallGroup": @1,
+    @"supportsVideo": @NO,
+  }];
+
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+ - (BOOL)application:(UIApplication *)application
+ continueUserActivity:(NSUserActivity *)userActivity
+   restorationHandler:(void(^)(NSArray * __nullable restorableObjects))restorationHandler
+ {
+   return [RNCallKeep application:application
+            continueUserActivity:userActivity
+              restorationHandler:restorationHandler];
+ }
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+// Linking API
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  return [super application:application openURL:url options:options] || [RCTLinkingManager application:application openURL:url options:options];
+}
+
+
+
+// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  return [super application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+  return [super application:application didFailToRegisterForRemoteNotificationsWithError:error];
+}
+
+// Explicitly define remote notification delegates to ensure compatibility with some third-party libraries
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+
+@end
